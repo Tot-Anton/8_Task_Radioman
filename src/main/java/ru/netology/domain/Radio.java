d51ede9d685974a1f26915e8a61c773b0974f8f2
Anton Belmisov <antonbelmisov@mail.ru>
2022-04-23 19:48:59 +0300

    added the flexible branch

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,19 +3,54 @@ package ru.netology.domain;
 public class Radio {
     private int currentPosition;
     private int currentVolume;
-    private int minPosition = 0;
+    private int minPosition;
     private int maxPosition = 9;
     private int minVolume = 0;
-    private int maxVolume = 10;
+    private int maxVolume = 100;
 
+    public int getMaxVolume() {
+        return maxVolume;
+    }
 
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
 
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getMinPosition() {
+        return minPosition;
+    }
+
+    public void setMinPosition(int minPosition) {
+        this.minPosition = minPosition;
+    }
+
+    public int getMaxPosition() {
+        return maxPosition;
+    }
+
+    public void setMaxPosition(int maxPosition) {
+        this.maxPosition = maxPosition;
+    }
 
     public int getCurrentPosition() {
         return currentPosition;
     }
 
     public void setCurrentPosition(int currentPosition) {
+        if (currentPosition < minPosition) {
+            return;
+        }
+        if (currentPosition > maxPosition) {
+            return;
+        }
         this.currentPosition = currentPosition;
     }
 
@@ -23,13 +58,13 @@ public class Radio {
         if (currentPosition < maxPosition) {
             currentPosition++;
         } else {
-            currentPosition = 0;
+            currentPosition = minPosition;
         }
 
     }
 
     public void prevRadioPosition() {  // выбор радиостанций назад
-        if (currentPosition > 0) {
+        if (currentPosition > minPosition) {
             currentPosition--;
         } else {
             currentPosition = maxPosition;
@@ -41,8 +76,8 @@ public class Radio {
             currentPosition = maxPosition;
             return;
         }
-        if (desiredRadioStation < 0) {
-            currentPosition = 0;
+        if (desiredRadioStation < minPosition) {
+            currentPosition = minPosition;
             return;
         }
         this.currentPosition = desiredRadioStation;
@@ -67,10 +102,10 @@ public class Radio {
     }
 
     public void loweringVolume() {  // понижение громкости
-        if (currentVolume > 0) {
+        if (currentVolume > minVolume) {
             currentVolume--;
         } else {
-            currentVolume = 0;
+            currentVolume = minVolume;
         }
     }
 
